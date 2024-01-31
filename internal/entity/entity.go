ce9ee07125555b49e2338e703f4df45c00c247ed
renatohvo <renatohvo@gmail.com>
2024-01-31 19:00:48 -0300

    product

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -13,3 +13,23 @@ func NewCategory(name string) *Category {
 		Name: name,
 	}
 }
+
+type Product struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	CategoryID  string
+	ImageURL    string
+}
+
+func NewProduct(name, description string, price float64, categoryID, imageURL string) *Product {
+	return &Product{
+		ID:          uuid.New().String(),
+		Name:        name,
+		Description: description,
+		Price:       price,
+		CategoryID:  categoryID,
+		ImageURL:    imageURL,
+	}
+}
